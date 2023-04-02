e7c64a9ec95a9150f27374081384b209fe4c0873
zzkevin <realzzkevin@gmail.com>
2023-04-01 23:30:00 -0400

    use react router

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,43 +1,54 @@
-import React, { useState } from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import About from './pages/About';
-import Works from './pages/Works';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume';
+import React, { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import About from "./pages/About";
+import Works from "./pages/Works";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
 
 export default function MainContainer() {
-    const [currentPage, setCurrentPage] = useState('About');
 
-    const renderPage = () => {
-        if (currentPage === 'About') {
-            return <About />;
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            elemenet: <div>HOME PAGE</>
+        },
+        {
+            paht: "/about",
+            element: <
         }
-        if (currentPage === 'Works') {
-            return <Works />;
-        }
-        if (currentPage === 'Contact') {
-            return <Contact />;
-        }
-        if (currentPage === 'Resume') {
-            return <Resume />;
-        }
+    ])
 
-        return <About />;
-    };
 
-    const handlePageChange = (page) => setCurrentPage(page);
+  const [currentPage, setCurrentPage] = useState("About");
 
-    return (
-        <div className="main">
-            <div className="content">
-                <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-                <div className="container-lg">
-                    {renderPage()}
-                </div>
-            </div>
-            <Footer />
-        </div>
-    );
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Works") {
+      return <Works />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    if (currentPage === "Resume") {
+      return <Resume />;
+    }
 
+    return <About />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div className="main">
+      <div className="content">
+        <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+        <div className="container-lg">{renderPage()}</div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
